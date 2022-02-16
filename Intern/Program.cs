257e4ed20316cf0285b71ff671d66a90d79f49d2
Marearcat <marearcat@gmail.com>
2022-02-16 21:40:25 +0400

    Добавьте файлы проекта.

---
+++ Intern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Intern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 20);
+                Console.Write(array[i] + " ");
+            }
+            System.Collections.ArrayList unique = new System.Collections.ArrayList();
+
+
+        }
+    }
+}
